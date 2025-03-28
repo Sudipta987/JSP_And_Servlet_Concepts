bf222663aa6bb3d260f225ed11ac11d04fb710ad
Sudipta987 <rajakar21162@gmail.com>
2025-03-28 13:57:35 +0530

    Q1 : This Implements the Get and Post Method Concepts in Servlet.

---
+++ Get_Post_Method/ImplementationGetPost.java
@@ -0,0 +1,23 @@
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/submit")
+public class ImplementationGetPost extends HttpServlet {	
+	
+	protected void doPost(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException{
+		PrintWriter out=res.getWriter();
+		String name=req.getParameter("name");
+		String email=req.getParameter("email");
+		String pass=req.getParameter("password");
+		if(email.equals("hunter@gmail.com") && pass.equals("hunter123")) {
+			out.println("Welcome,"+name);
+		}else {
+			out.println("Wrong Credentials,Please Enter Correct Password or email!");
+		}
+	}
+}
